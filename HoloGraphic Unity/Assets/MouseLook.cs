a3b8fcfdb5daf82bd9401ba3ba647dc3ce6582e7
Ricky102 <48187498+Ricky102@users.noreply.github.com>
2022-10-03 03:23:49 -0400

    Fixed inverted y axis movement

--- HoloGraphic Unity/Assets/MouseLook.cs
+++ HoloGraphic Unity/Assets/MouseLook.cs
@@ -8,11 +8,10 @@ public class MouseLook : MonoBehaviour
 
     public float mouseSensitivityX;
     public float mouseSensitivityY;
-
     public Transform orientation;
-
     float xRotation;
     float yRotation;
+    public bool flipY = true;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +23,7 @@ public class MouseLook : MonoBehaviour
     void Update()
     {
         float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivityX * Time.deltaTime;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivityY * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivityY * Time.deltaTime * (flipY ? -1 : 1);
         //turn.x = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         //turn.y = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
@@ -34,6 +33,6 @@ public class MouseLook : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
-        //playerBody.Rotate(Vector3.up * turn.x);
+        //playerBody.Rotate(Vector3.up * xRotation);
     }
 }
