f3589b10305aba1bf558a45a497a1e5e3b9a4bfe
Ricky102 <48187498+Ricky102@users.noreply.github.com>
2022-10-03 03:16:50 -0400

    X, Y axes camera movement added
    Added basic camera movement on x and y axes. Need to fix issue where camera is looking behind the player and yaxis movement is inverted.

---
+++ HoloGraphic Unity/Assets/MouseLook.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public Transform playerBody;
+
+    public float mouseSensitivityX;
+    public float mouseSensitivityY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivityX * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivityY * Time.deltaTime;
+        //turn.x = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        //turn.y = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation += mouseY;
+        yRotation += mouseX;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        //playerBody.Rotate(Vector3.up * turn.x);
+    }
+}
