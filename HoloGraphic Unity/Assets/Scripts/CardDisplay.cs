b35adfb5f796e27e735a9220a4e46972dc300d17
Ricky102 <48187498+Ricky102@users.noreply.github.com>
2022-10-11 03:14:23 -0400

    Card UI updates with card data
    Added code to CardDisplay script so that we can pass scriptable object in along with all its data. Update card data and changes will be reflected in UI.

--- HoloGraphic Unity/Assets/Scripts/CardDisplay.cs
+++ HoloGraphic Unity/Assets/Scripts/CardDisplay.cs
@@ -2,22 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CardDisplay : MonoBehaviour
 {
     public Card card;
 
-    public Text nameText;
-    public Text descriptionText;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI descriptionText;
     //public Image artworkImage;
-    public Text memoryText;
-    public Text attackText;
-    public Text defenseText;
-    public Text healthText;
+    public TextMeshProUGUI memoryText;
+    public TextMeshProUGUI attackText;
+    //public TextMeshProUGUI defenseText;
+    public TextMeshProUGUI healthText;
 
     // Start is called before the first frame update
     void Start()
     {
-        card.Print();
+        //card.Print();
+        nameText.text = card.name;
+        descriptionText.text = card.description;
+
+        //artworkImage.sprite
+
+        memoryText.text = card.memoryCost.ToString();
+        attackText.text = card.attack.ToString();
+        healthText.text = card.health.ToString();
     }
 }
